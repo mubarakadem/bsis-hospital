821b4db8dd6fa475aec02051be4020b7d045acae
mubarakadem <mubarakadem8@gmail.com>
2022-10-20 11:09:25 -0700

    go_router router field added

---
+++ lib/navigtion/go_router.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(routes: [
+  GoRoute(path: "/", builder: (BuildContext context, GoRouterState state) => HomeScreen())
+]);
